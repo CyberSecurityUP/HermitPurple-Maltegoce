8cdfc3174eeb07cf72a13aae6fe19f9344895a6c
Joas A Santos <34966120+CyberSecurityUP@users.noreply.github.com>
2024-05-17 16:49:49 -0300

    Update SearchMissingPerson.py

--- transforms/SearchMissingPerson.py
+++ transforms/SearchMissingPerson.py
@@ -6,7 +6,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 class SearchMissingPerson(DiscoverableTransform):
     """
-    Transform to search for missing persons from desaparecidosdobrasil.org and the FBI based on name.
+    Transform to search for missing persons from multiple sources based on name.
     """
 
     @classmethod
@@ -14,18 +14,14 @@ class SearchMissingPerson(DiscoverableTransform):
         name_to_search = request.Value.strip().lower()
         response.addUIMessage(f"Searching for: {name_to_search}")
 
-        with ThreadPoolExecutor(max_workers=2) as executor:
-            # Submit search tasks to the executor
-            future_desaparecidos = executor.submit(cls.search_desaparecidos, name_to_search, response)
-            future_fbi = executor.submit(cls.search_fbi, name_to_search, response)
-            
-            # Wait for both tasks to complete
-            future_desaparecidos.result()
-            future_fbi.result()
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            executor.submit(cls.search_desaparecidos, name_to_search, response)
+            executor.submit(cls.search_fbi, name_to_search, response)
+            executor.submit(cls.search_garda, name_to_search, response)
 
     @staticmethod
     def search_desaparecidos(name_to_search, response):
-        max_id = 300
+        max_id = 150
         base_url = "https://desaparecidosdobrasil.org/pesquisa_exibecadastrodesaparecidos.php?edit_id={}"
         for i in range(1, max_id + 1):
             url = base_url.format(i)
@@ -57,7 +53,23 @@ class SearchMissingPerson(DiscoverableTransform):
                     entity = response.addEntity('maltego.Website', link)
                     entity.addProperty('URL', 'URL', 'strict', link)
 
+    @staticmethod
+    def search_garda(name_to_search, response):
+        url = "https://www.garda.ie/en/missing-persons/"
+        response_api = requests.get(url)
+        response.addUIMessage(f"Checking Garda URL: {url} - Status: {response_api.status_code}")
+
+        if response_api.status_code == 200:
+            soup = BeautifulSoup(response_api.text, 'html.parser')
+            persons = soup.find_all('li', class_='missing-person')
+            
+            for person in persons:
+                h2_tag = person.find('h2')
+                if h2_tag:
+                    name = h2_tag.text.strip()
+                    if name_to_search in name.lower():
+                        person_entity = response.addEntity('maltego.Person', name)
+
 if __name__ == "__main__":
     from maltego_trx.server import serve_transform_classes
     serve_transform_classes([SearchMissingPerson])
-                                                     
