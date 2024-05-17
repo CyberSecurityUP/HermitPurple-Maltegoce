a1e5dc8c79381bf2384c36220b5b8b7d5500247a
Joas A Santos <34966120+CyberSecurityUP@users.noreply.github.com>
2024-05-17 00:52:20 -0300

    Update SearchMissingPerson.py

--- transforms/SearchMissingPerson.py
+++ transforms/SearchMissingPerson.py
@@ -2,21 +2,31 @@ from maltego_trx.maltego import MaltegoMsg, MaltegoTransform
 from maltego_trx.transform import DiscoverableTransform
 import requests
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
 class SearchMissingPerson(DiscoverableTransform):
     """
-    Transform to search for missing persons from desaparecidosdobrasil.org based on name.
-    Includes full name, location of disappearance, and optionally, the police report number.
+    Transform to search for missing persons from desaparecidosdobrasil.org and the FBI based on name.
     """
 
     @classmethod
     def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
-        name_to_search = request.Value.strip().lower()  # Ensure case insensitivity and strip extra spaces
+        name_to_search = request.Value.strip().lower()
         response.addUIMessage(f"Searching for: {name_to_search}")
-        max_id = 350
-        
-        base_url = "https://desaparecidosdobrasil.org/pesquisa_exibecadastrodesaparecidos.php?edit_id={}"
 
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            # Submit search tasks to the executor
+            future_desaparecidos = executor.submit(cls.search_desaparecidos, name_to_search, response)
+            future_fbi = executor.submit(cls.search_fbi, name_to_search, response)
+            
+            # Wait for both tasks to complete
+            future_desaparecidos.result()
+            future_fbi.result()
+
+    @staticmethod
+    def search_desaparecidos(name_to_search, response):
+        max_id = 300
+        base_url = "https://desaparecidosdobrasil.org/pesquisa_exibecadastrodesaparecidos.php?edit_id={}"
         for i in range(1, max_id + 1):
             url = base_url.format(i)
             response_api = requests.get(url)
@@ -28,24 +38,26 @@ class SearchMissingPerson(DiscoverableTransform):
                 
                 if input_name and input_name.get('value'):
                     full_name = input_name.get('value').strip().lower()
-                    response.addUIMessage(f"Found Name: {full_name}")
-
                     if name_to_search in full_name:
-                        response.addUIMessage("Match found, adding entity...")
                         entity = response.addEntity('maltego.Person', full_name.title())
 
-                        local_desaparecimento = soup.find('input', {'id': 'txtlocaldesaparecimento'})
-                        if local_desaparecimento and local_desaparecimento.get('value'):
-                            entity.addProperty('disappearanceLocation', 'Local Desaparecimento', 'loose', local_desaparecimento.get('value'))
-                        
-                        # Collect and add the police report number if available
-                        boletim_ocorrencia = soup.find('input', {'id': 'txtboletimocorrencia'})
-                        if boletim_ocorrencia and boletim_ocorrencia.get('value'):
-                            boletim_number = boletim_ocorrencia.get('value').strip()
-                            response.addEntity('maltego.Phrase', boletim_number)
-                            response.addUIMessage(f"Police Report Number: {boletim_number}")
+    @staticmethod
+    def search_fbi(name_to_search, response):
+        url = "https://www.fbi.gov/wanted/kidnap"
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response_api = requests.get(url, headers=headers)
+        response.addUIMessage(f"Checking FBI URL: {url} - Status: {response_api.status_code}")
+
+        if response_api.status_code == 200:
+            soup = BeautifulSoup(response_api.text, 'html.parser')
+            for item in soup.find_all('li', class_="portal-type-person"):
+                title = item.find('h3', class_='title')
+                if title and name_to_search in title.text.lower():
+                    link = title.find('a')['href']
+                    entity = response.addEntity('maltego.Website', link)
+                    entity.addProperty('URL', 'URL', 'strict', link)
 
 if __name__ == "__main__":
     from maltego_trx.server import serve_transform_classes
     serve_transform_classes([SearchMissingPerson])
-
+                                                     
